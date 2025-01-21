059deb388892ba92e0ccaac82bab7829f53594f6
MirasXX1 <mirasshokybay06@gmail.com>
2025-01-21 14:08:45 +0500

    Delete modify.py

--- modify.py
+++
@@ -1,8 +0,0 @@
-a = "Hello, World!"
-print(a.upper())
-print(a.lower())
-print(a.replace("H", "J"))
-print(a.split(",")) # returns ['Hello', ' World!']
-
-a = " Hello, World! "
-print(a.strip()) # returns "Hello, World!"
